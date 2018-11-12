824947b056f42b6b7a9167cfb0ce52e1c7983478
RowanHowell <30895206+RowanHowell@users.noreply.github.com>
2018-11-12 09:53:12 +0000

    Add files via upload

--- SPIdataShell.R
+++ SPIdataShell.R
@@ -4,17 +4,15 @@ library(tidyverse)
 source("SPIscripts.R") # Load functions into R
 
 
-GOIfile = "AbcGOIcontrol.txt" # Ensure name is in quotes and has file extension
-GBPfile = "AbcGBPcontrol.txt"
-plasmid = "pHT999"
-writename = "Abc1data.csv" # This must end in .csv!
+GOIfile = "Nud1GOIcontrol.txt" # Ensure name is in quotes and has file extension
+GBPfile = "Nud1GBPcontrol.txt"
+plasmid = "pHT584"
+writename = "Nud1data.csv" # This must end in .csv!
 
-H = AnalyseSPIData(GOIfile,GBPfile,plasmid,write = TRUE, writename, smooth = TRUE)
+H = AnalyseSPIData(GOIfile,GBPfile,plasmid,write = TRUE, writename, smooth = T, smoothLGR = T, aggregate  =F)
 # Your data is now stored in the data frame H and has been written to file
 
 SPIplots(H)
 # THis will make a plot showing correlation of controls and spread of data
 
-CheckSmoothing(H)
-# This will make a plot showing the plate effects for each of the comparisons before and after smoothing
-# Note you may need to increase the height of the plot window in Rstudio if you see " Error in plot.new() : figure margins too large "
+
